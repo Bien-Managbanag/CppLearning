16a4ff50ac62e5095e7f9b2f5ed5065ff4bd9061
Bien Managbanag <bien.managbanag2@mail.dcu.ie>
2023-10-09 00:19:06 +0100

    added string learning file

--- cpp_learning/string.cpp
+++ cpp_learning/string.cpp
@@ -5,7 +5,10 @@ using namespace std;
 int main(){
     string phrase = "I love pizza";
     cout << "Hello\n"; /*using \n can put the string on the next line */
+    phrase[0] = 'B'; /*changes pos[0] in phrase from I to B*/
     cout << phrase << endl;
 
+    cout << phrase.find("pizza", 0) << endl; /*find function finds a certain string in phras;  pizza starts at index pos 7*/
+    cout << phrase.substr(3, 6) << endl; /*(x,y) x = starting index, y = ending index */
     return 0;
 }
