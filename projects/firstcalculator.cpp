c28af64507d39cadd25247a11064b9ff9864fb5d
Bien Managbanag <bien.managbanag2@mail.dcu.ie>
2023-10-11 15:45:51 +0100

    added files to learning and added first project

---
+++ projects/firstcalculator.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int num1, num2;
+    /*double num1, num2*/
+    /*for floating point numbers*/
+
+    cout << "Enter first number: ";
+    cin >> num1;
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    cout << num1 + num2;
+
+    return 0;
+}
